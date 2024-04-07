6fb69a355f628d489a596defb14adc8cadb26033
om <kmcamja@gmail.com>
2024-04-07 09:08:48 +0530

    commit😂💯🐶❤️❤️🐍

--- script/download.js
+++ script/download.js
@@ -3,7 +3,7 @@ document.querySelector(".btn").addEventListener("click", function () {
   var cvUrl = './Om_Chaudhari_CV.pdf';
   var link = document.createElement("a");
   link.href = cvUrl;
-  link.download = "oc_CV.pdf";
+  link.download = "Om CV.pdf";
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
